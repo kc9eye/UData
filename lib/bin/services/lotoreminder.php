8bda3c263ae19c9e5283383c194a48e857d497bb
Paul Lane <kc9eye@outlook.com>
2021-10-14 12:40:12 -0500

    loto annual reminder

---
+++ lib/bin/services/lotoreminder.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright (C) 2021 Paul W. Lane <kc9eye@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class review75init implements Service {
+    private $server;
+    private $data;
+
+    public function __construct (Instance $server) {
+        $this->server = $server;
+        $this->data = array();
+    }
+
+    public function cronjob () {
+        return true;
+    }
+
+    public function kill () {
+        return true;
+    }
+
+    public function run() {
+        $notifier = new Notification($this->server->pdo,$this->server->mailer);
+        $body = $this->server->mailer->wrapInTemplate('lotoreminder.html',"");
+        $notifier->notify("LOTO Reminder","LOTO Annual Review DUE",$body);
+        return true;
+    }
+}
