4320671cd35f40ef0180bf05a3fe5cb59f34c5f8
Paul Lane <kc9eye@outlook.com>
2021-10-14 12:42:26 -0500

    bug

--- lib/bin/services/lotoreminder.php
+++ lib/bin/services/lotoreminder.php
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-class review75init implements Service {
+class lotoreminder implements Service {
     private $server;
     private $data;
 
