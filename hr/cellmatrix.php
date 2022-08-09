58b8c316c921dd2bdebf52f808e817c5826ec7d2
Paul Lane <kc9eye@outlook.com>
2022-08-09 13:20:20 -0500

    adams entry

---
+++ hr/cellmatrix.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright (C) 2022 Paul W. Lane <kc9eye@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+require_once(dirname(__DIR__).'/lib/init.php');
+
+if (!empty($_REQUEST['action'])) {
+    switch($_REQUEST['action']) {
+        default:
+            editMatrix();
+        break;
+    }
+}
+else
+    editMatrix();
+
+function editMatrix() {
+    global $server;
+    include('submenu.php');
+    $server->userMustHavePermission('viewProfiles');
+    $emp = new Employee($server->pdo,$_REQUEST['id']);
+    $view = $server->getViewer('HR: Edit Matrix');
+    $view->sideDropDownMenu($submenu);
+    $view->h1("Matrix for: <small>".$emp->getFullName()."</small>");
+
+    $view->footer();
+}
