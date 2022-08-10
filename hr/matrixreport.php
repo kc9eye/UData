25193d2b196f0a05a4af7c007abf49d9d06d3367
Paul Lane <kc9eye@outlook.com>
2022-08-10 11:07:54 -0500

    dev

--- hr/matrixreport.php
+++ hr/matrixreport.php
@@ -29,43 +29,43 @@ function displayReport() {
     global $server;
     $server->userMustHavePermission('viewProfiles');
     $view = $server->getViewer("Employee Matrix");
-    $view->wrapInPre(var_export(getData(),true));
+    // $view->wrapInPre(var_export(getData(),true));
     $view->footer();
 }
 
-function getData() {
-    global $server;
-    $sql = 
-    'select 
-        profiles.first,
-        profiles.middle,
-        profiles.last,
-        employees.id
-    from employees
-    inner join profiles on profiles.id = employees.pid
-    where employees.end_date is null
-    order by profiles.last asc';
-    try {
-        $pntr = $server->pdo->prepare($sql);
-        if (!$pntr->execute()) throw new Exception(print_r($pntr->errorInfo(),true));
-        $emps = $pntr->fetchAll(PDO::FETCH_ASSOC);
-    }
-    catch(Exception $e) {
-        trigger_error($e->getMessage(),E_USER_WARNING);
-        return "NOPE";
-    }
+// function getData() {
+//     global $server;
+//     $sql = 
+//     'select 
+//         profiles.first,
+//         profiles.middle,
+//         profiles.last,
+//         employees.id
+//     from employees
+//     inner join profiles on profiles.id = employees.pid
+//     where employees.end_date is null
+//     order by profiles.last asc';
+//     try {
+//         $pntr = $server->pdo->prepare($sql);
+//         if (!$pntr->execute()) throw new Exception(print_r($pntr->errorInfo(),true));
+//         $emps = $pntr->fetchAll(PDO::FETCH_ASSOC);
+//     }
+//     catch(Exception $e) {
+//         trigger_error($e->getMessage(),E_USER_WARNING);
+//         return "NOPE";
+//     }
 
-    $sql =
-    'select *
-    from cell_matrix
-    where eid = ?
-    order by gen_date desc
-    limit 1';
-    $pntr = $server->pdo->prepare($sql);
-    foreach($emps as $row) {
-        try {
-            if (!$pntr->execute($row['id'])) throw new Exception(print_r($pntr->errorInfo(),true));
+//     $sql =
+//     'select *
+//     from cell_matrix
+//     where eid = ?
+//     order by gen_date desc
+//     limit 1';
+//     $pntr = $server->pdo->prepare($sql);
+//     foreach($emps as $row) {
+//         try {
+//             if (!$pntr->execute($row['id'])) throw new Exception(print_r($pntr->errorInfo(),true));
             
-        }
-    }
+//         }
+//     }
 }
