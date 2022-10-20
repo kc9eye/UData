5688df4448fb268ecf02bcc3aec35c3b5df68f9a
Paul Lane <kc9eye@outlook.com>
2022-10-20 12:50:08 -0500

    bug

--- hr/submenu.php
+++ hr/submenu.php
@@ -21,6 +21,6 @@ $submenu = [
     'Employee Injuries'=>$server->config['application-root'].'/hr/incidents',
     'Employee Time'=>$server->config['application-root'].'/hr/time',
     'Attendance Points'=>$server->config['application-root'].'/hr/points',
-    'Attendance Occurrences'=>$server->config['application-root'].'/hr/attendanceoccurences',
+    'Attendance Occurrences'=>$server->config['application-root'].'/hr/attendanceoccurrences',
     'Employee Matrix'=>$server->config['application-root'].'/hr/matrixreport'
 ];
