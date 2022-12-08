aa8649fce59fbe1423afdf110d25949d3f5312c5
Paul Lane <kc9eye@outlook.com>
2022-12-08 07:50:15 -0600

    bg

--- hr/attendance.php
+++ hr/attendance.php
@@ -155,11 +155,19 @@ function addAttendanceRecord() {
         </div>';
         exit();
     }
-    if (($_REQUEST['begin_date_range'] != "") && ($_REQUEST['end_date_range'] == "")) {
+    else if (($_REQUEST['begin_date_range'] != "") && ($_REQUEST['end_date_range'] == "")) {
         echo
         '<div class="border border-secondary rounded m-3">
             <h4 class="bg-danger">Error</h4>
             <b>An end date is required for a date range entry.</b>&#160;
+            <a href="'.$server->config['application-root'].'/hr/attendance?id='.$_REQUEST['eid'].'" class="btn btn-danger m-1" role="button">Try Again</a>
+        </div>';
+        exit();
+    }
+    else if (($_REQUEST['occ_date'] == "") && ($_REQUEST['begin_date_range'] == "") && ($_REQUEST['end_date_range'])) {
+        '<div class="border border-secondary rounded m-3">
+            <h4 class="bg-danger">Error</h4>
+            <b>A date is required for any entry</b>&#160;
             <a href="'.$server->config['application-root'].'/hr/attendance?id='.$_REQUEST['eid'].'" class="btn btn-danger m-1" role="button">Try Again</a>
         </div>';
         exit();
