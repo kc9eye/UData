74674ce86782bfa5760454417f71c8f583dc05aa
Paul Lane <kc9eye@outlook.com>
2023-06-13 11:09:33 -0500

    dbg

--- lib/init.php
+++ lib/init.php
@@ -43,7 +43,7 @@ require_once(dirname(__DIR__).'/framework/errorhandler.php');
 $loader = new Autoloader(dirname(__DIR__),['.php','.lib.php','.class.php','.obj.php','.int.php']);
 die("I loaded things!");
 $error = new Errorhandler($config['error-log-file-path'], $config['error-support-link']);
-
+die("I initialized the error handler!");
 
 
 if (empty($_SESSION['viewer'])) {
