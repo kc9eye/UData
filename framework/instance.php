941fde0d059e6e89fe68ad1025ae408cdb4c192c
Paul W. Lane <kc9eye@gmail.com>
2025-08-27 12:16:01 -0500

    8.4 deprecated implict nullable

--- framework/instance.php
+++ framework/instance.php
@@ -33,49 +33,49 @@ Class Instance {
     const DIALOG_UNFINISHED = 1024;
     const DIALOG_FINISHED = 4096;
     const DIALOG_ERROR = 2112;
-    
+
     /**
      * Contains the publicly accessible configuraiton array
-     * 
+     *
      * @var Array $config
      */
     public $config;
 
     /**
      * Contains the publicly accessible PDO object
-     * 
+     *
      * @var PDO $pdo
      */
     public $pdo;
 
     /**
      * Contains the publicly accessible security object
-     * 
+     *
      * @var Security $security
      */
     public $security;
 
     /**
      * Contains the publicly accessible mailer object
-     * 
+     *
      * @var Mailer $mailer
      */
     public $mailer;
 
     /**
      * Contains the current user ID
-     * 
+     *
      * @var String $currentUserID Null if no valid users
      */
     public $currentUserID;
 
     /**
      * The class constructor
-     * 
-     * Creates the most needed objects for models and controllers and stores them 
+     *
+     * Creates the most needed objects for models and controllers and stores them
      * in properties
-     * 
-     * @param Array $config Should be the configuration array. 
+     *
+     * @param Array $config Should be the configuration array.
      * @return Object Instance
      * @see config.php
      * @author Paul W. Lane
@@ -91,18 +91,18 @@ Class Instance {
 
     /**
      * Instantiates the ViewMaker object and outputs UI controller interface stub.
-     * 
+     *
      * Initializes the ViewMaker object with all the required objects and outputs
      * the initial framework UI interface. It is up to you to end that interface stub
      * with a `$myobjectvar->footer()` method call. This method also returns the
      * ViewMaker object. It should be called thusly: `$view = $server->getViewer('My Page Title')`.
-     * 
+     *
      * @param String $pagetitle Optional pagetitle for the browser page. Defaults to configuration option
      * `$config['company-name']`.
      * @param Array $pagedata Optional array of PageData for content manipulation.
      * @return ViewMaker The ViewMaker Object used to start the page.
      * @uses ViewMaker
-     * 
+     *
      * @author Paul W. Lane
      */
     public function getViewer ($pagetitle = null, $pagedata = null) {
@@ -118,19 +118,19 @@ Class Instance {
         $view->header();
         return $view;
     }
-    
+
     /**
      * Outputs an entire HTML interface with debugging information.
-     * 
+     *
      * This method will ouput string given encapsulated in a scrollable `<pre>`
      * section, wrapped in an entire standard framework interface to the output stream.
      * It is intended to be used for debugging controllers.
-     * 
+     *
      * @param String $debug Optional, anything passed in this variable as string will be output
      * to the stream.
-     * 
+     *
      * @return void
-     * 
+     *
      * @author Paul W. Lane
      */
     public function getDebugViewer ($debug = '') {
@@ -151,7 +151,7 @@ Class Instance {
 
     /**
      * Is called by the constructor to instantiate the `Security` model and object
-     * 
+     *
      * @return Object Security
      * @author Paul W. Lane
      */
@@ -172,17 +172,17 @@ Class Instance {
 
     /**
      * Outputs a message to the stream for the end user wrapped in an interface.
-     * 
+     *
      * Outputs a standard interface message the end user via the `$message` paramater.
-     * The interface can be styled in one of two ways using the `$success` parameter. The interface 
+     * The interface can be styled in one of two ways using the `$success` parameter. The interface
      * will also display a click button that will redirect the user to any hyperlink given
      * using the `$target_link` parameter.
-     * 
+     *
      * @param String $message The message to be displayed to the user.
      * @param Boolean $success Optional, defaults to `FALSE` indicating a failure styling.
      * @param String $target_link Optional, hyperlink to redirect the user to,
      * defaults to `$config['application-root']`.
-     * 
+     *
      * @author Paul W. Lane
      */
     public function newEndUserDialog ($message = '', $success = DIALOG_FAILURE, $target_link = null) {
@@ -214,23 +214,23 @@ Class Instance {
 
     /**
      * Produces a processing dialog for the user while a background handler is called.
-     * 
-     * This method produces a feedback dialog while the given handler is initiated to 
-     * process users request. It essentially lets your handler run in the background 
-     * preventing the users from clicky, clicky while their request is processed by the 
+     *
+     * This method produces a feedback dialog while the given handler is initiated to
+     * process users request. It essentially lets your handler run in the background
+     * preventing the users from clicky, clicky while their request is processed by the
      * handler. The given handler must return `boolean true` upon successful completion.
      * @return Void The process ends with either a successful dialog or failure dialog,
-     * depending on the return of the handler. The user will return to the API given in 
+     * depending on the return of the handler. The user will return to the API given in
      * the `$click` parameter or the main API URL by default.
-     * @param Mixed $handler The handler given should be in the form as would be given to 
+     * @param Mixed $handler The handler given should be in the form as would be given to
      * the `call_user_func_array` ({@link http://php.net/manual/en/function.call-user-func-array.php}).
-     * @param Array $args The arguments to the handler in the same form as given to 
+     * @param Array $args The arguments to the handler in the same form as given to
      * `call_user_func_array` ({@link http://php.net/manual/en/function.call-user-func-array.php}).
      * @param String $click The URL of the API to give the user once the end dialog is reached.
      * @param String $feedback Optional, The feedback to display to the user during the dialog.
      * @author Paul W. lane
      */
-    public function processingDialog ($handler = null, Array $args = null, $click = null, $feedback = null) {
+    public function processingDialog ($handler = null, Array $args = [], $click = null, $feedback = null) {
         $dialog = function($feedback){
             $feedback = is_null($feedback) ? 'Processing' : $feedback;
             $view = $this->getViewer('Processing...');
@@ -313,20 +313,20 @@ Class Instance {
                     unset($_SESSOIN['bg_process']);
                     $this->newEndUserDialog("An Unkown Error Occurred.",DIALOG_FAILURE,$url);
                 break;
-            }            
+            }
         }
         die();
      }
 
     /**
      * Determines if the current user is valid and logged in.
-     * 
+     *
      * This method is used in controllers to determine whether the current
-     * user is logged in and that the log in is valid. It returns true if the 
+     * user is logged in and that the log in is valid. It returns true if the
      * user is logged in and valid or outputs the `notAuthorized` interface
      * and returns false otherwise. This is a must have method. This method
      * can not return false as execution is halted if it fails.
-     * 
+     *
      * @return Mixed Boolean|Void
      * @see Instance::notAuthorized()
      * @author Paul W. lane
@@ -342,13 +342,13 @@ Class Instance {
 
     /**
      * Checks whether or not the user has a specific permission.
-     * 
-     * The `Security` model is queried and a boolean is returned 
+     *
+     * The `Security` model is queried and a boolean is returned
      * to indicate whether of not the current user has the given
      * permission. The method outputs the `notAuthorized()` interface
      * to the stream on failure. This is a must have method. It can not
      * return false as execution is halted if it fails.
-     * 
+     *
      * @param String $perm The permission to check against the current user
      * @return Mixed Boolean|Void
      * @see Instance::notAuthorized()
@@ -368,14 +368,14 @@ Class Instance {
 
     /**
      * Checks whether the current user has the given role.
-     * 
-     * The `Security` model is queried as to whether the current user 
-     * has the specified role. Returns true if the user has the 
+     *
+     * The `Security` model is queried as to whether the current user
+     * has the specified role. Returns true if the user has the
      * specified role or the user has the role of 'Administrator'.
      * Otherwise returns false and outputs the `notAuthorized()` interface
      * to the stream. This is a must have method. It can not return false
      * as execution is halted if it fails.
-     * 
+     *
      * @param String $role The role to check the current user against
      * @return Mixed Boolean|Void
      * @see Instance::notAuthorized()
@@ -396,9 +396,9 @@ Class Instance {
 
     /**
      * Determines if the current user has a role.
-     * 
+     *
      * If the current user has the given role or has the role
-     * of Adminstrator; returns true, otherwise false. 
+     * of Adminstrator; returns true, otherwise false.
      * This is a shortcut method to the Security model.
      * @param String $role The role to check if the users has.
      * @return Boolean True if user has the role, false otherwise.
@@ -428,11 +428,11 @@ Class Instance {
 
     /**
      * Checks whether the current user has the given permission
-     * 
+     *
      * Check whether or not the current user has the given permission
-     * and returns true on success, or false on failure. Unlike the 
+     * and returns true on success, or false on failure. Unlike the
      * must have methods, this method does not terminate execution.
-     * 
+     *
      * @param String $permission The permission to check the user for
      * @return Boolean
      * @author Paul W. Lane
@@ -456,18 +456,18 @@ Class Instance {
 
     /**
      * Checks a given array of strings against the current users permissions
-     * 
+     *
      * Given an unindexed array of permission strings, with the optional `$and`
      * parameter set to true then *all* permissions in the given array must be in the
      * users permission set. Otherwise, will only succeed if *any* of the permissions
      * are in the users permission set.
-     * 
+     *
      * @param Array $perms The unindexed array of permissions to check
-     * @param Boolean $and If set to true requires all given permissions in the array be present in the 
+     * @param Boolean $and If set to true requires all given permissions in the array be present in the
      * users permission set.
      * @return Boolean
      * @author Paul W. Lane
-     * 
+     *
      */
     public function checkPermsArray (Array $perms, $and = false) {
         $aggregate = 0;
@@ -495,12 +495,12 @@ Class Instance {
 
     /**
      * Stops script execution and outputs a not authorized interface to the stream.
-     * 
-     * This method stops current script execution after ouputing an interface to the 
+     *
+     * This method stops current script execution after ouputing an interface to the
      * stream styled as not authorized.
      * @param Boolean $escalation Optional boolean indicating that this is an attempted user privilege escalation
      * @return Void
-     * @author Paul W. Lane 
+     * @author Paul W. Lane
      */
     public function notAuthorized ($escalation = false) {
         $buff = ob_get_contents();
@@ -512,10 +512,10 @@ Class Instance {
 
     /**
      * Outputs a 404 error to the stream.
-     * 
-     * Method outputs a 404 error styled interface to the stream and 
+     *
+     * Method outputs a 404 error styled interface to the stream and
      * stops script execution.
-     * 
+     *
      * @return Void
      * @author Paul W. Lane
      */
@@ -536,16 +536,16 @@ Class Instance {
 
     /**
      * Represents a sane way to redirect to other links.
-     * 
+     *
      * This method is used to redirect to other links in a sane manner,
      * with little code. It must be called prior to any other output
      * to the stream. Given a relative path link it uses the `$config['application-root']`
-     * configuration variable as preamble to the `$link` parameter. Otherwise setting the 
+     * configuration variable as preamble to the `$link` parameter. Otherwise setting the
      * optional paramter of `$offsite` to `true` will cause the link given to be untouched.
-     * 
+     *
      * @param String $link The link to redirect to.
      * @param Boolean $offsite Whether or not the `$link` should be preambled with the `$config['application-root']` variable.
-     * 
+     *
      * @return Void
      * @author Paul W. Lane
      */
@@ -566,11 +566,11 @@ Class Instance {
 
     /**
      * Used by user/login to redirect back to a requested previously unauthorized page
-     * 
+     *
      * This method is used by `Instance->notAuthorized()` to return a redirect URL
-     * to `user/login`, so the user will be returned to a previously unauthorized 
+     * to `user/login`, so the user will be returned to a previously unauthorized
      * request after a succesful login.
-     * 
+     *
      * @author Paul W. Lane
      * @param String $url Optional url to give to the `user/login` controller
      * @return Boolean Always returns `true`
@@ -586,7 +586,7 @@ Class Instance {
             $uri = $url;
         }
         if ($cnt > 3) $uri = '/'.$uri;
-        
+
         $_SESSION['login-redirect'] = $uri;
         return true;
     }
