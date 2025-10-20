fb9bd4119a7786e145035fdee339efe192988a68
Paul W. Lane <kc9eye@gmail.com>
2025-10-20 06:37:14 -0500

    doc update

--- safety/hazcomdoc.php
+++ safety/hazcomdoc.php
@@ -30,13 +30,7 @@ function main(){
         $view->linkButton('','Edit Document',null,false,'_blank',true);
     }
     $view->hr();
-    echo '<iframe 
-        id="printFrame" 
-        name="printFrame" 
-        src=""
-        width="800"
-        height="600"
-        ></iframe>';
+    echo '<iframe src="https://docs.google.com/document/d/e/2PACX-1vRhWzhM-YNaJrMR84Fg9aeUYC18e_dyCOCkuJRBqM5UgHlqT6EYKcd2mfNhPudbFiDXLZ-wkDtLjDMy/pub?embedded=true"></iframe>';
     $view->footer();
 }
 // $doc = new DocumentViewer($server);
@@ -47,18 +41,18 @@ function main(){
 
 // if (!empty($_REQUEST)) {
 //     switch($_REQUEST['action']) {
-//         case 'edit': 
-//             $doc->editDisplay($submenu) or $server->notAuthorized(); 
+//         case 'edit':
+//             $doc->editDisplay($submenu) or $server->notAuthorized();
 //         break;
-//         case 'approve': 
-//             $doc->approveDisplay($submenu) or $server->notAuthorized(); 
+//         case 'approve':
+//             $doc->approveDisplay($submenu) or $server->notAuthorized();
 //         break;
-//         case 'submit': 
+//         case 'submit':
 //             $doc->submitForApproval($_REQUEST)
 //             && $server->newEndUserDialog('Document submitted for approval',DIALOG_SUCCESS,$doc->docURL)
 //             or $server->newEndUserDialog('Something went wrong, you may not have access to do this',DIALOG_FAILURE,$doc->docURL);
 //         break;
-//         case 'submitapproval': 
+//         case 'submitapproval':
 //             $doc->approvalGranted($_REQUEST)
 //             && $server->newEndUserDialog('Document edition was approved',DIALOG_SUCCESS,$doc->docURL)
 //             or $server->newEndUserDialog('Something went wrong, you may not have access to do this',DIALOG_FAILURE,$doc->docURL);
@@ -67,8 +61,8 @@ function main(){
 //             && $server->newEndUserDialog('Document was rejected',DIALOG_SUCCESS,$doc->docURL)
 //             or $server->newEndUserDialog('Something went wrong, you may not have access to do this',DIALOG_FAILURE,$doc->docURL);
 //         break;
-//         default: 
-//             $doc->displayDoc($submenu); 
+//         default:
+//             $doc->displayDoc($submenu);
 //         break;
 //     }
 // }
