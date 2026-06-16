a9c74a23fd6b335b7fcdca223d22aa47939ec446
Paul W. Lane <kc9eye@gmail.com>
2026-06-16 10:17:09 -0500

    debug

--- products/editbom.php
+++ products/editbom.php
@@ -41,18 +41,20 @@ if (!empty($_REQUEST['action'])) {
         break;
         case 'add':
             $bom = new BillOfMaterials($server->pdo);
-            if (!$bom->verifyMaterialExists($_REQUEST['number']))
-                $server->newEndUserDialog(
-                    "This material number does not yet exist, first add it as a valid material.",
-                    DIALOG_FAILURE,
-                    $server->config['application-root'].'/material/addnew'
-                );
-            else
-                $server->processingDialog(
-                    [$bom,'addendumBOM'],
-                    [$_REQUEST],
-                    $server->config['application-root'].'/products/bom?prokey='.$_REQUEST['prokey']
-                );
+            echo "<pre>",var_export($bom->verifyMaterialExists($_REQUEST['number'])),"</pre>";
+            exit();
+            // if (!$bom->verifyMaterialExists($_REQUEST['number']))
+            //     $server->newEndUserDialog(
+            //         "This material number does not yet exist, first add it as a valid material.",
+            //         DIALOG_FAILURE,
+            //         $server->config['application-root'].'/material/addnew'
+            //     );
+            // else
+            //     $server->processingDialog(
+            //         [$bom,'addendumBOM'],
+            //         [$_REQUEST],
+            //         $server->config['application-root'].'/products/bom?prokey='.$_REQUEST['prokey']
+            //     );
         break;
         case 'rerebase':
             $_REQUEST['file'] = new FileUpload(FileIndexer::UPLOAD_NAME);
